391e0673f0523757947937c299f27318959bbd98
Foxel <foxel@quickfox.ru>
2012-03-01 12:11:53 +0700

    fixed index.php

--- core/SOne/Request.php
+++ core/SOne/Request.php
@@ -4,7 +4,7 @@ class SOne_Request extends FBaseClass
 {
     public function __construct(K3_Environment $env)
     {
-        list ($path) = explode('?', preg_replace('#^index\.php/#i', '', $env->requestUrl), 2);
+        list ($path) = explode('?', preg_replace('#^index\.php/?#i', '', $env->requestUrl), 2);
         $query = $env->request->getURLParams();
         $action = null;
         if (reset($query) === '') { // for queries like foo/bar?edit
