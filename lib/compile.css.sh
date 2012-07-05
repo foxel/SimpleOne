e6ef2c08ddfd6cbc4cf545fd41a47bc2fe9d7350
Foxel <foxel@quickfox.ru>
2012-07-05 17:06:17 +0700

    Updated jQuery-UI bootstrap style

--- lib/compile.css.sh
+++ lib/compile.css.sh
@@ -3,3 +3,5 @@
 lessc less/bootstrap/bootstrap.less > ../static/css/bootstrap.css
 lessc less/bootstrap/responsive.less > ../static/css/bootstrap-responsive.css
 lessc less/bootstrap/font-awesome-ie7.less > ../static/css/font-awesome-ie7.css
+
+lessc less/style.less > ../static/css/simple.css
