b1b9d42ba0b0a6e55a02e4504a8e7cd56a5ca3bf
Foxel <foxel@quickfox.ru>
2014-05-12 13:57:07 +0700

    updated to use blobs for data storing

---
+++ sql/patch-2014-05-12.sql
@@ -0,0 +1,5 @@
+--  Changeset updates.2014.05::data_binary::Foxel::(Checksum: 3:b8d6773d0222b87625193d1f60883df3)
+ALTER TABLE qfso_objects_data MODIFY data LONGBLOB;
+
+-- INSERT INTO `DATABASECHANGELOG` (`AUTHOR`, `COMMENTS`, `DATEEXECUTED`, `DESCRIPTION`, `EXECTYPE`, `FILENAME`, `ID`, `LIQUIBASE`, `MD5SUM`, `ORDEREXECUTED`) VALUES ('Foxel', '', NOW(), 'Custom SQL', 'EXECUTED', 'updates.2014.05', 'data_binary', '2.0.5', '3:b8d6773d0222b87625193d1f60883df3', 30);
+
